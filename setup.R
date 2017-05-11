dc67a7a8971963025eed93ec4b132620988a53ed
ulrich <panoramadata@gmail.com>
2017-05-11 16:44:51 -0400

    Adds setup doc
    file path is bad for collaboration

---
+++ setup.R
@@ -0,0 +1,17 @@
+## SETUP AND FUNCTIONS -------------------------------------------------
+
+# Change to your file path
+setwd(“~/uatz/git/rbubbles-in-3d”)
+
+# List of packages for session
+packages <- c( "dplyr", "ggplot2", "readr")
+
+# Install CRAN packages (if not already installed)
+.inst <- packages %in% installed.packages()
+if(length(packages[!.inst]) > 0) install.packages(packages[!.inst])
+
+# Load packages into session 
+sapply(packages, require, character.only = TRUE)
+
+# ggplot2 options
+theme_set(theme_minimal(base_family = "Helvetica Neue", base_size = 14))
